55c9dc4b4e7cd2fa90cb0258172a8e4e12bd2c0b
gasmoya1994 <gasmoya1994@gmail.com>
2024-04-05 13:41:12 -0300

    navegación dinámica con tablas

--- public/nav.php
+++ public/nav.php
@@ -1,9 +1,16 @@
 <?php
-$ENLACES=array(
-    "cerrar_sesion" => session_abort(),
-    "capacitaciones" => "cursos",
-    "alumnos" => "usuarios"
-);
+include "config/dbconn.php";
+$TABLAS=$DBCONN->query("show tables");
+function listartablas($ARRAY){
+    while($ROW=$ARRAY->fetch_assoc()){
+        echo "
+        <li>
+            <a class='nav-link' href='?tabla=".$ROW["Tables_in_escuela"]."'>"
+            .$ROW["Tables_in_escuela"].
+            "</a> 
+        </li>";
+    }
+} 
 
 ?>
 <nav class="navbar navbar-dark navbar-expand-md bg-dark p-2">
@@ -11,9 +18,7 @@ $ENLACES=array(
     <button data-bs-target=".navbar-nav" data-bs-toggle="collapse" class="navbar-toggler navbar-toggler-icon"></button>
     <ul class="navbar-nav navbar-collapse collapse">
         <?php
-        foreach($ENLACES as $KEY => $VALUE){
-            echo "<li> <a class='nav-link' href='$VALUE'>$KEY</a></li>";
-        }
+        listartablas($TABLAS);
         ?>
     </ul>
 </nav>
