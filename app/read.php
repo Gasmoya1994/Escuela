925334dfa5fa244ddf20c3903b9dd494728e036a
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-04-23 13:15:01 -0300

    actualizacion de estilos de tabla

--- app/read.php
+++ app/read.php
@@ -7,14 +7,20 @@ function encabezados($ARRAY){
     while($ROW=$ARRAY->fetch_assoc()){
         echo "<th>".$ROW["Field"]."</th>";
     }
+    echo "<th>acciones</th>";
 }
 function cuerpotabla($ARRAY){
     if($ARRAY->num_rows>0){
         while($ROW=$ARRAY->fetch_assoc()){
             echo "<tr>";
-            foreach($ROW as $VALUE)
-            echo "<td> $VALUE </td>";
-            echo "</tr>";
+            foreach($ROW as $VALUE) echo "<td> $VALUE </td>";
+            $id = isset($ROW['id']) ? 'id' : 'ID';
+            echo "
+            <td>
+                <button data-id=".$ROW[$id]." class='btn btn-primary'>editar</button>
+                <button data-id=".$ROW[$id]." class='btn btn-danger'>eliminar</button>
+            </td>
+            </tr>";
         }
     }
     else{
@@ -22,7 +28,7 @@ function cuerpotabla($ARRAY){
     }
 }
 ?>
-<table>
+<table class="table table-dark table-striped">
     <caption>Registros de <?php echo $TABLA ?> </caption>
     <thead>
         <?php encabezados($COLUMNAS) ?>
